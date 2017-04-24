1904b2ea57096e76787c7f2c262e04d030826a05
robneal <robneal28@gmail.com>
2017-04-24 11:31:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Hello Web App
+Simple Django Collection of things (blog, products, etc) project 
+
+---
+
+Author(s): Robert Neal
+
+Description: Simple Django Collection of things (blog, products, etc) project  
+
+Project Status : In Development
+
+Staic files Link - https://robneal.github.io/hellowebapp
+
+Digital Ocean Link - 
---
+++ collection/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request): 
+	number = 6
+	# don't forget the quotes because it's a string,
+	# not an integer
+	thing = "Thing name"
+	return render (request, 'index.html', {
+		'number': number,
+		# don't forget to pass it in, and the last comma
+		'thing': thing,
+	})
