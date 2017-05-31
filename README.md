936b165018026c44bece8f12bca3d677e63a4f89
robneal <robneal28@gmail.com>
2017-05-30 22:04:31 -0400

    finished chapter 12

--- README.md
+++ README.md
@@ -14,4 +14,14 @@ Static files Link - https://robneal.github.io/hellowebapp
 Digital Ocean VPS Link - 
 
 ### Virtual Environment Installations
+	from directory myhellowebapp $
+	
+	- pip install virtualenv
+	- virtualenv venv
+	- source venv/bin/activate
+	- pip install Django==1.9.6
 	- pip install django-registration-redux==1.3
+	- python manage.py migrate
+	- python manage.py runserver
+
+For more help checkout this doc : https://hellowebapp.com/setup/
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -17,7 +17,10 @@ from collection import views
 from collection.backends import MyRegistrationView
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
+# added RedirectView to this import statement
+from django.views.generic import (TemplateView, 
+    RedirectView,
+)
 from django.contrib.auth.views import (
    password_reset, 
    password_reset_done,
@@ -31,6 +34,8 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     
+
+    url(r'^things/$', RedirectView.as_view(pattern_name='browse', permanent=True)),
     url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, name='thing_detail'),
 
     url(r'^things/(?P<slug>[-\w]+)/edit/$', views.edit_thing, name='edit_thing'),
@@ -38,6 +43,12 @@ urlpatterns = [
 
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'), # I edited
     url(r'^accounts/create_thing/$', views.create_thing, name='registration_create_thing'),
+
+
+    # our new redirect view
+    url(r'^browse/$', RedirectView.as_view(pattern_name='browse', permanent=True)),
+    url(r'^browse/name/$', views.browse_by_name, name='browse'),
+    url(r'^browse/name/(?P<initial>[-\w]+)/$', views.browse_by_name, name='browse_by_name'),
     
     # the new password reset URLs
     url(r'^accounts/password/reset/$', 
